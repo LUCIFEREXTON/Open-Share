13a60f7af021cac6c8fdf0cae9d96448f50b6506
LUCIFEREXTON <57142378+LUCIFEREXTON@users.noreply.github.com>
2021-10-23 09:50:00 +0530

    Update secrets.js

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,6 +1,6 @@
 /* eslint-disable eol-last */
 module.exports = {
-    // dbURI: 'mongodb+srv://P2dppOAbUfXjf5HM:P2dppOAbUfXjf5HM@cluster0.vdwiq.mongodb.net/socialmedia?retryWrites=true&w=majority',
-    dbURI: 'mongodb://127.0.0.1:27017/socialmedia',
+    dbURI: 'mongodb+srv://P2dppOAbUfXjf5HM:P2dppOAbUfXjf5HM@cluster0.vdwiq.mongodb.net/socialmedia?retryWrites=true&w=majority',
+    //dbURI: 'mongodb://127.0.0.1:27017/socialmedia',
     jwtKey: 'abrakadabra'
 };
